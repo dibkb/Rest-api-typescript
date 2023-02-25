60fe1b570fd22e166a6edaae8edd12ca8e4b2668
dibkb <dibas9110@gmail.con>
2023-02-25 21:57:32 +0530

    user signup controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,1 +1,29 @@
-
+import bcrypt from "bcrypt";
+import { createUser, getUserByEmail, getUserById } from "db/Users/Users";
+import express from "express";
+export const signUp = async (req: express.Request, res: express.Response) => {
+  const { username, email, password } = req.body;
+  try {
+    if (!username || !email || !password) {
+      res.status(400).json({ message: "Fields is empty" });
+    }
+    //   check if email is taken
+    const userCheck = await getUserByEmail(email);
+    if (userCheck) {
+      res.status(400).json("Emmail already exists");
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const newUser = await createUser({
+        username,
+        email,
+        authentication: {
+          hashedPassword,
+        },
+      });
+      res.status(200).json(newUser);
+    }
+  } catch (error) {
+    res.status(400).json(error.message).end();
+  }
+};
